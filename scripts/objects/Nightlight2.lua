09719a337df462bc2f98d2f1b2b7f34d0042ca84
mknauer2005-bit <mknauer2005@gmail.com>
2026-04-03 15:53:11 +0300

    Add files via upload

---
+++ scripts/objects/Nightlight2.lua
@@ -0,0 +1,15 @@
+-- Copyright (C) GIANTS Software GmbH, Confidential, All Rights Reserved.
+
+
+
+
+
+
+
+
+
+---Creating nightlight object
+-- @param integer id ID of the node
+function Nightlight2:onCreate(id)
+    Logging.warning("i3d onCreate user-attribute 'Nightlight2' is deprecated. Please use 'Visibility Condition'-Tab in GIANTS Editor for node '%s' instead", getName(id))
+end
